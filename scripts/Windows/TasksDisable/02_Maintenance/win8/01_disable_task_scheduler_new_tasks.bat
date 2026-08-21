4f4cec13f31639aa9207666caa5e95abe4f9ebcc
andry81 <andry@inbox.ru>
2026-08-21 17:42:26 +0300

    2026.08.21: * changed: scripts/Windows/TasksDisable/02_Maintenance/win?/01_disable_task_scheduler_new_tasks.bat: switched to use standalone enable/disable scripts

--- scripts/Windows/TasksDisable/02_Maintenance/win8/01_disable_task_scheduler_new_tasks.bat
+++ scripts/Windows/TasksDisable/02_Maintenance/win8/01_disable_task_scheduler_new_tasks.bat
@@ -2,39 +2,9 @@
 
 rem Description:
 rem   Script prevents the Task Scheduler of a new task creation for everyone.
-rem
-rem   Based on:
-rem     https://superuser.com/questions/1222551/how-to-prevent-programs-from-making-new-entries-in-task-scheduler-when-installin/1237127#1237127
 
 setlocal
 
 call "%%~dp0..\__init__\__init__.bat"
 
-rem script names call stack, disabled due to self call and partial inheritance (process elevation does not inherit a parent process variables by default)
-rem if defined ?~ ( set "?~=%?~%-^>%~nx0" ) else if defined ?~nx0 ( set "?~=%?~nx0%-^>%~nx0" ) else set "?~=%~nx0"
-set "?~=%~nx0"
-
-if 0%IMPL_MODE% NEQ 0 goto IMPL
-set "PSEXEC=%CONTOOLS_ADMIN_PROJECT_EXTERNALS_ROOT%/sysinternals/psexec.exe"
-"%USERBIN_SCRIPTS_BAT_ROOT%/runas/hta/cmd-admin-system.bat" /c @set "IMPL_MODE=1" ^& "%~f0" %*
-exit /b
-
-:IMPL
-call "%%CONTOOLS_ROOT%%/std/is_system_elevated.bat" || (
-  echo;%?~%: error: process must be System account elevated to continue.
-  exit /b 255
-) >&2
-
-call :CMD "%%SystemRoot%%\System32\icacls.exe" "%%SystemRoot%%\System32\Tasks" /deny "*S-1-1-0:(WD)"
-
-echo;
-
-pause
-
-exit /b
-
-:CMD
-echo;^>%*
-(
-  %*
-)
+call "%%CONTOOLS_ADMIN_PROJECT_ROOT%%/scripts/Windows/TaskSched/disable_tasksched_new_tasks.bat" %%*
